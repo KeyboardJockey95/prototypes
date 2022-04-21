644bf42ec1061cc9a10fafde762b8a141cd8f2e0
KeyboardJockey95 <102330912+KeyboardJockey95@users.noreply.github.com>
2022-04-21 12:24:53 -0600

    Starting footstep for MTCDict

---
+++ MTC/MTCDict/MTCDict.cs
@@ -0,0 +1,21 @@
+﻿/// <summary>
+/// 
+/// File: MTCDict.cs
+/// 
+/// Author: Mike Eldredge
+/// 
+/// Origination Date 21 Apr 2022
+/// 
+/// Description:
+///     Import a dictionary entry from its native format
+///     Instantiate a DictionaryEntry object
+///     Export the object as XML
+/// 
+/// Sample Input
+/// 
+/// Sample Output
+/// 
+///     Hello, world!
+///     
+/// </summary>
+Console.WriteLine("Hello, World!");
